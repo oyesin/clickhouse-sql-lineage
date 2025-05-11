2f2490960a30c7dba211d09f5d071496fd09a2f0
away <away.animal@gmail.com>
2025-05-11 11:49:11 +0800

    feat: 添加错误捕获组件

--- lineage-web/src/main.tsx
+++ lineage-web/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
+import ErrorBoundary from './components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
